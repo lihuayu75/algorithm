bdb50949d0dc9c608188dc689542107e6fa1099b
huali <huali@ebay.com>
2022-05-02 22:51:45 -0400

    find duplicate with H/T

--- findtheduplicate/Case1.java
+++ findtheduplicate/Case1.java
@@ -1,5 +1,7 @@
 package findtheduplicate;
 
+import java.util.*;
+
 /**
  * Giving an arry cantais n+1 elements between 1 and n inclusive, find the duplicate element (assume it can appear more than once)
  * -there is only one duplicate element
@@ -14,5 +16,25 @@ package findtheduplicate;
  * duplicate: 2
  */
 public class Case1 {
-    
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 1, 5, 2, 2};
+        int i = findDuplicate(arr);
+        System.out.println("duplicate number is: " + i);
+    }
+
+    public static int findDuplicate(int[] arr){
+        // your code here
+        int t = arr[0];
+        int h = arr[arr[0]];
+        while (t != h) {
+            t = arr[t];
+            h = arr[arr[h]];
+        }
+        t = 0;
+        while (t != h) {
+            t = arr[t];
+            h = arr[h];
+        }
+        return t;
+    }
 }
